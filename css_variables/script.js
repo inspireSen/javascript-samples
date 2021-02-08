8422366d2ba577ddc7f70c3896d59f20029a5723
Jason Chen <cfg815@gmail.com>
2021-02-07 22:17:04 -0500

    control css variables

--- css_variables/script.js
+++ css_variables/script.js
@@ -1,2 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(event) {    
+document.addEventListener('DOMContentLoaded', function (event) {
+    const inputs = document.querySelectorAll('.controls input');
+    inputs.forEach(input => input.addEventListener('change', handleUpdate))
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
 })
+
+
+
+function handleUpdate(e) {
+    console.log(e)
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
